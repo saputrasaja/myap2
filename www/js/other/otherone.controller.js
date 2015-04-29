d3c716ff787c2644b093c96435138d97d78f2325
Ron Panduwana <panduwana@gmail.com>
2015-04-29 10:11:51 +0700

    new file

---
+++ www/js/other/otherone.controller.js
@@ -0,0 +1,14 @@
+(function() {
+	"use strict";
+
+	angular
+		.module("app.other")
+		.controller("OtherOneCtrl", OtherOneCtrl);
+
+		function OtherOneCtrl($scope, $ionicModal) {
+			activate();
+			function activate() {
+			}
+			
+		}
+})();
